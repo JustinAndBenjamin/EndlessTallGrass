b9f97a5426e94d65ef6a7fd5e8858db961e4c503
Benjamin-306 <github@benjamin.fudeus.net>
2026-09-04 19:36:28 +0200

    Speechbubble Improvements (#128)
    Little improvment on speechbubble function; text now selectable in NPC Node, var full_time now same length of Audio Stream (-> Audio Node in SpeechBubble Scene)

--- script/npc.gd
+++ script/npc.gd
@@ -2,6 +2,11 @@ extends CharacterBody2D
 
 @onready var speech_bubble = $SpeechBubble
 
+@export var text : String
+
+func _ready() -> void:
+	speech_bubble.text = text
+
 func _on_speech_trigger_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 			speech_bubble.speech()
--- script/speech.gd
+++ script/speech.gd
@@ -1,9 +1,16 @@
 extends Control
 
-@export var full_time : float
-@export var text : String
-
+@onready var Audio = $Audio
+@onready var full_time : float
+@onready var text : String
 @onready var canvas_layer = $CanvasLayer
+
+func _ready() -> void:
+	if Audio.stream:
+		full_time = Audio.stream.get_length()
+	else:
+		full_time = 0.0 # Standardwert, falls kein Ton existiert
+
 
 func speech() -> void:
 	canvas_layer.show()
@@ -11,6 +18,7 @@ func speech() -> void:
 	var text_length = text.length()
 	var wait_time : float = full_time / text_length
 	
+	Audio.play()
 	for buchstabe in text:
 		$CanvasLayer/Text.text += str(buchstabe)
 		var timer = get_tree().create_timer(wait_time)
