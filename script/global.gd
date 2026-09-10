71fd9a2ba89aae41aad2ce3babab9e8f2ec24cd4
Benjamin-306 <github@benjamin.fudeus.net>
2026-09-10 21:08:11 +0200

    Textures in inv are loaded now (#150)

--- script/global.gd
+++ script/global.gd
@@ -1,6 +1,6 @@
 extends Node
 
 var geladene_position: Vector2
-var inventar : Array
+var inventar : Dictionary
 var hat_position: bool = false
 var hat_inventar : bool = false
--- script/inventory.gd
+++ script/inventory.gd
@@ -1,6 +1,6 @@
 extends Control
 
-var inventory = [null, null, null, null, null, null, null, null]
+var inventory = {}
 var full = bool(false)
 
 func _ready() -> void:
@@ -9,23 +9,28 @@ func _ready() -> void:
 		Global.hat_inventar = false
 
 func _process(delta: float) -> void:
-	var i = 0
-	for object in inventory:
-		i += 1
-		if typeof(object) == TYPE_STRING:
+	for i in inventory:
+		var load_text = inventory[i].keys()[0]
+		var load_image = inventory[i][load_text]
+		if typeof(load_text) == TYPE_STRING:
 			var direction_label = get_node("Monpok" + str(i) + "/Label")
-			direction_label.text = object
+			direction_label.text = load_text
+		var direction_rect = get_node("Monpok" + str(i) + "/TextureRect")
+		direction_rect.texture = load_image
 	
 func add(TEXT, IMAGE):
 	full = false
-	var free_place = inventory.find(null)
-	if free_place != -1:
-		free_place += 1
-		var direction_rect = get_node("Monpok" + str(free_place) + "/TextureRect")
-		var direction_label = get_node("Monpok" + str(free_place) + "/Label")
+	var count : int = 1
+	for i in inventory:
+		count +=1
+	if count <= 10:
+		var direction_rect = get_node("Monpok" + str(count) + "/TextureRect")
+		var direction_label = get_node("Monpok" + str(count) + "/Label")
 		direction_rect.texture = IMAGE
 		direction_label.text = TEXT
-		inventory[free_place - 1] = TEXT
+		inventory[count] = {
+			TEXT : IMAGE
+		} 
 	else:
 		full = true
 		$Timer.start()
@@ -47,7 +52,7 @@ func _on_clear_inventory_pressed() -> void:
 			direction_rect.texture = null
 		if direction_label:
 			direction_label.text = ""
-	inventory = [null, null, null, null, null, null, null, null]
+	inventory = {}
 		
 		
 		
